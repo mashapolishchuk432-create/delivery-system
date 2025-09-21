6cb636e1c23a65d186fedb591eee0ac619dec9da
Hanak-ko <106978793+Hanak-ko@users.noreply.github.com>
2025-09-21 22:08:34 +0300

    Update Route.java
    Commited by daria

--- src/main/java/edu/vntu/Route.java
+++ src/main/java/edu/vntu/Route.java
@@ -1,42 +1,39 @@
-package edu.vntu;
+class Route {
+    int id;
+    String startLocation;
+    String endLocation;
+    double distance;
+    String estimatedTime;
+    String trafficStatus;
+    String[] checkpoints;
+    Vehicle vehicle;
+    Courier courier;
+    String status;
 
-public class Route {
-        int id;
-        String startLocation;
-        String endLocation;
-        double distance;
-        String estimatedTime;
-        String trafficStatus;
-        String[] checkpoints;
-        Vehicle vehicle;
-        Courier courier;
-        String status;
-
-        public Route(int id, String startLocation, String endLocation) {
-            this.id = id;
-            this.startLocation = startLocation;
-            this.endLocation = endLocation;
-        }
-
-        public void calculateTime() {
-            System.out.println("Estimated time: " + estimatedTime);
-        }
-
-        public void optimize() {
-            System.out.println("Route optimized.");
-        }
-
-        public void updateStatus(String status) {
-            this.status = status;
-        }
-
-        public void addCheckpoint(String checkpoint) {
-            // simplified
-            System.out.println("Checkpoint added: " + checkpoint);
-        }
-
-        public void printRoute() {
-            System.out.println("Route from " + startLocation + " to " + endLocation);
-        }
+    public Route(int id, String startLocation, String endLocation) {
+        this.id = id;
+        this.startLocation = startLocation;
+        this.endLocation = endLocation;
     }
+
+    public void calculateTime() {
+        System.out.println("Estimated time: " + estimatedTime);
+    }
+
+    public void optimize() {
+        System.out.println("Route optimized.");
+    }
+
+    public void updateStatus(String status) {
+        this.status = status;
+    }
+
+    public void addCheckpoint(String checkpoint) {
+        System.out.println("Checkpoint added: " + checkpoint);
+    }
+
+    public void printRoute() {
+        System.out.println("Route from " + startLocation + " to " + endLocation);
+    }
+}
 
